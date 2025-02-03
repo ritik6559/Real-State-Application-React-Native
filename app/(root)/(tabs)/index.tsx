7c4e5b99314a06b48ef64f5dc074ab79cb1f6c85
ritik6559 <ritikjoshi741@gmail.com>
2025-02-03 22:56:39 +0530

    profile screen completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,26 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <SafeAreaView className='bg-white h-full' >
+        <View className='px-5' >
+            <View className='flex flex-row items-center justify-between mt-5' >
+                <View className='flex flex-row items-center'>
+                    <Image source={images.avatar} className='size-12 rounded-full' />
+                    <View className='flex flex-col items-start ml-2 justify-center' >
+                       <Text className='tet-sx font-rubik text-black-100' >Good Morning</Text>
+                       <Text className='text-base font-rubik-medium text-black-300' >Ritik Joshi</Text>
+                    </View>
+                </View>
 
-    <Link href={'/sign-in'} >signin</Link>
-        <Text className="font-bold text-lg my-10" >Hello</Text>
-      <Text>app/index.tsx to edit this screen.</Text>
-    </View>
+                <Image source={icons.bell} className='size-6' />
+
+            </View>
+        </View>
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,51 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, ImageSourcePropType, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import icons from '@/constants/icons';
 import images from '@/constants/images';
+import { settings } from '@/constants/data';
+import { useGlobalContext } from '@/lib/global-provider';
+import { logout } from '@/lib/appwrite';
+
+
+interface SettingsItemProps {
+    icon: ImageSourcePropType;
+    title: string;
+    onPress?: () => void;
+    textStyle?: any;
+    showArrow?: boolean
+}
+
+
+const SettingsItem = ({ icon, title, onPress, textStyle, showArrow = true } : SettingsItemProps) => (
+    <TouchableOpacity onPress={onPress} className='flex flex-row items-center justify-between py-3' >
+        <View className='flex flex-row items-center gap-3'>
+            <Image source={icon} className='size-5' />
+            <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`} >{title}</Text>
+        </View>
+
+    {showArrow && <Image source={icons.rightArrow} className='size-5' />}
+
+    </TouchableOpacity>
+)
+
+
 
 const profile = () => {
 
-    const handleLogin = async () => {};
+
+    const { user, refetch } = useGlobalContext();
+
+    const handleLogout = async () => {
+        const result = await logout();
+
+        if(result){
+            Alert.alert("Success", "You have been logged out successfully" );
+            refetch();
+        } else {
+            Alert.alert("Error", "An error occurred while logging out")
+        }
+    };
 
 
 
@@ -23,20 +62,33 @@ const profile = () => {
             <Image source={icons.bell} className='size-5' />
         </View>
 
-        <View className='flex-row justify-center flex mt-5' >
+        <View className='flex flex-row justify-center mt-5' >
             <View className='flex flex-col items-center relative mt-5' >
-                <Image source={images.avatar} className='size-44 relative rounded-full'  />
+                <Image source={{uri: user?.avatar}} className='size-44 relative rounded-full'  />
             
                 <TouchableOpacity className='absolute bottom-11 right-2' >
                     <Image source={icons.edit} className='size-9' />
 
-
-                    <Text className='text-3xl font-rubik-bold mt-2' >Ritik</Text>
-
                 </TouchableOpacity>
+                    <Text className='text-3xl font-rubik-bold mt-2' >{user?.name}</Text>
             </View>
         </View>
+        
+        <View className='flex flex-col mt-10' >
+            <SettingsItem icon={icons.calendar} title='My Bokkings' />
+            <SettingsItem icon={icons.wallet} title='Payments' />
+        </View>
 
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200' >
+            {settings.slice(2).map((item, index) =>(
+                <SettingsItem key={index} {...item} />
+            )) }
+        </View>
+
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200' >
+            <SettingsItem icon={icons.logout} title='Logout' textStyle="text-danger" showArrow={false} onPress={handleLogout} />
+
+        </View>
 
       </ScrollView>
     </SafeAreaView>
