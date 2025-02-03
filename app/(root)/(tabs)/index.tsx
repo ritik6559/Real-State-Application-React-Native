cbc5de9b81a7167049e827327d63b1a17a285da4
ritik6559 <ritikjoshi741@gmail.com>
2025-02-04 01:56:57 +0530

    home screen ui completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,12 +1,16 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className='bg-white h-full' >
+    <ScrollView>
         <View className='px-5' >
             <View className='flex flex-row items-center justify-between mt-5' >
                 <View className='flex flex-row items-center'>
@@ -18,9 +22,41 @@ export default function Index() {
                 </View>
 
                 <Image source={icons.bell} className='size-6' />
-
             </View>
+
+               <Search />
+        
+        <View className='my-5' >
+            <View className='flex flex-row items-center justify-between'>
+                    <Text className='text-xl font-rubik-bold text-black-300' >Featured</Text>
+                    <TouchableOpacity>
+                        <Text className='text-base font-rubik-bold text-primary-300' >See All</Text>
+                    </TouchableOpacity>
         </View>
-    </SafeAreaView>
+        <View className='flex flex-row gap-5 mt-5' >
+            <FeaturedCard/>
+            <FeaturedCard/>
+            <FeaturedCard/>
+        </View>
+    
+
+        <View className='flex flex-row items-center justify-between mt-5' >
+            <Text className='text-xl font-rubik-bold text-balck-300' >Our Recommendation</Text>
+            <TouchableOpacity className='text-base font-rubik-bold text-primary-300' >
+                <Text>See All</Text>
+            </TouchableOpacity>
+        </View>
+    
+        <Filters/>
+
+        <View className='flex flex-row map-5 mt-5' >
+            <Card/>
+            <Card />
+        </View>
+
+        </View>
+        </View>
+        </ScrollView>
+        </SafeAreaView>
   );
 }
