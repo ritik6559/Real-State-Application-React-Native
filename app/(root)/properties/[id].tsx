265aae6e0a09798ac8dcb4fe043c5b38dc5d2962
ritik6559 <ritikjoshi741@gmail.com>
2025-02-10 09:34:55 +0530

    adding property details screen

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,15 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions, Image } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { useAppwrite } from '@/lib/useAppwrite';
+import { getPropertyById } from '@/lib/appwrite';
+import { ScrollView } from 'react-native-gesture-handler';
+import images from '@/constants/images';
 
 const Property  = () => {
 
+    const { id } = useLocalSearchParams<{ id?: string }>();
 
-    const { id } = useLocalSearchParams();
+    const windowsHeight = Dimensions.get("window").height;
+
+
+    const { data: property } = useAppwrite({
+        fn: getPropertyById,
+        params: {
+            id: id!,
+        }
+    });
+
+
 
   return (
     <View>
-      <Text>Property </Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 bg-white"
+      >
+        <View className='relative w-full' style={{ height: windowsHeight / 2 }} >
+            <Image 
+                source={{
+                    uri: property?.image
+                }}
+                className='size-full'
+                resizeMode="cover"
+            />
+
+            <Image
+                source={ images.whiteGradient }
+                className='absolute top-0 w-full z-40'
+            />
+        </View>
+        
+      </ScrollView>
     </View>
   )
 }
