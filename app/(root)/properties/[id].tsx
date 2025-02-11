b5413fd20e00fe29b30d1aa3ca8d0a039756c686
ritik6559 <ritikjoshi741@gmail.com>
2025-02-12 00:02:14 +0530

    completed

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, Dimensions, Image, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, Dimensions, Image, Platform, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useAppwrite } from '@/lib/useAppwrite';
@@ -6,6 +6,9 @@ import { getPropertyById } from '@/lib/appwrite';
 import { ScrollView } from 'react-native-gesture-handler';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import { facilities } from '@/constants/data';
+import Comment from '@/components/Comment'
+
 
 const Property  = () => {
 
@@ -134,6 +137,105 @@ const Property  = () => {
               {property?.description}
             </Text>
           </View>
+
+          <View className='mt-5 px-5 ' >
+                <Text className='text-black-300 text-xl font-rubik-bold' >Facilities</Text>
+          
+                { property?.facilities.length > 0 && (
+                    <View className='flex flex-row flex-wrap items-start justify-start t-2 gap-2' >
+                        {
+                            property?.facilities.map((item: string, index: number) => {
+                                const facility = facilities.find((facility) => facility.title === item)
+                                
+                                return (
+                                    <View
+                                        key={index}
+                                        className='flex flex-1 flex-col items-center min-w-16 max-w-20 '
+                                    >
+                                            <View className="size-14 bg-primary-100 rounded-full flex items-center justify-center">
+                                            <Image
+                                                source={facility ? facility.icon : icons.info}
+                                                className="size-6"
+                                            />
+                                        </View>
+
+                                        <Text
+                                            numberOfLines={1}
+                                            ellipsizeMode="tail"
+                                            className="text-black-300 text-sm text-center font-rubik mt-1.5"
+                                        >
+                                            {item}
+                                        </Text>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                )}
+         </View>
+
+          {property?.gallery.length > 0 && (
+            <View className="mt-7 px-5">
+              <Text className="text-black-300 text-xl font-rubik-bold">
+                Gallery
+              </Text>
+              <FlatList
+                contentContainerStyle={{ paddingRight: 20 }}
+                data={property?.gallery}
+                keyExtractor={(item) => item.$id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <Image
+                    source={{ uri: item.image }}
+                    className="size-40 rounded-xl"
+                  />
+                )}
+                contentContainerClassName="flex gap-4 mt-3"
+              />
+            </View>
+          )}
+
+          <View className="mt-7 px-5">
+            <Text className="text-black-300 text-xl font-rubik-bold">
+              Location
+            </Text>
+            <View className="flex flex-row items-center justify-start mt-4 gap-2">
+              <Image source={icons.location} className="w-7 h-7" />
+              <Text className="text-black-200 text-sm font-rubik-medium">
+                {property?.address}
+              </Text>
+            </View>
+
+            <Image
+              source={images.map}
+              className="h-52 w-full mt-5 rounded-xl"
+            />
+          </View>
+
+          {property?.reviews.length > 0 && (
+            <View className="mt-7 px-5 ">
+              <View className="flex flex-row items-center justify-between">
+                <View className="flex flex-row items-center">
+                  <Image source={icons.star} className="size-6" />
+                  <Text className="text-black-300 text-xl font-rubik-bold ml-2">
+                    {property?.rating} ({property?.reviews.length} reviews)
+                  </Text>
+                </View>
+
+                <TouchableOpacity>
+                  <Text className="text-primary-300 text-base font-rubik-bold">
+                    View All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <View className="mt-5">
+                <Comment item={property?.reviews[0]} />
+              </View>
+            </View>
+          )}
+        
       </ScrollView>
 
 
